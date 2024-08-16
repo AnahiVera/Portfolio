f02aabe6d43759665b89a5e9653b047f634679e9
AnahiVera <168015268+AnahiVera@users.noreply.github.com>
2024-08-16 16:30:58 +0000

    update

--- data/user-data.js
+++ data/user-data.js
@@ -1,13 +1,10 @@
 export const userData = {
-  githubUser: 'said7388',
-  devUsername: "said7388",
+  githubUser: 'AnahiVera',
+  devUsername: "Anahi Vera",
   github: 'https://github.com/said7388',
-  facebook: 'https://www.facebook.com/abusaid.riyaz/',
-  linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-  twitter: 'https://twitter.com/said7388',
-  stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-  leetcode: "https://leetcode.com/said3812/",
+  facebook: 'https://www.facebook.com/Tiburoncin.Uaha?mibextid=ZbWKwL',
+  linkedIn: 'www.linkedin.com/in/anahi-vera-rogel-b2b0812a9',
   resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing",
-  skills: ['React', 'NextJS', 'Redux', 'Express', 'NestJS', 'MySql', 'MongoDB', 'Postgres', 'Docker', 'AWS'],
-  timezone: '+6'
+  skills: ['BootStrap', 'JS'],
+  timezone: '-4'
 };
